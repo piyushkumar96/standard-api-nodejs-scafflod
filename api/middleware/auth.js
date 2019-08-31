6db5a06fdf3172b83154428f848dca504135444d
Piyush Kumar <piyush25032@gmail.com>
2019-08-31 16:31:27 +0530

    auth middleware

---
+++ api/middleware/auth.js
@@ -0,0 +1,34 @@
+/*
+ *This file is for authenticating user to hit APIs
+*/
+
+'use strict';
+
+// External Modules
+const   jwt = require('jsonwebtoken');
+
+// Internal Modules
+const   userSchema = require('../models/users/usersModel'),
+        config = require('../../config.json'),
+        logger = require('../../logger');
+
+const   loggerName    = "[authMiddleware ]: ";
+
+exports.auth = async (req, res, next) => {
+    try {
+        const token = req.header('Authorization').replace('Bearer ', '')
+        const decoded = jwt.verify(token, config.jwt_secret)
+        const user  = await userSchema.findOne({ _id: decoded._id , 'tokens.token':token})
+
+        if(!user){
+            throw new Error()
+        }
+
+        req.token = token
+        req.user = user
+        next()
+    }catch(err){
+        logger.error(loggerName + err)
+        res.status(401).send({error : "Please Authenticate First"})
+    }
+}
