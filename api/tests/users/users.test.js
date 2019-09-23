71b7cdeb34438499789c421967b6799af5aa39e0
Piyush Kumar <piyush25032@gmail.com>
2019-09-23 14:31:40 +0530

    delete user test added

--- api/tests/users/users.test.js
+++ api/tests/users/users.test.js
@@ -16,11 +16,14 @@ beforeEach(setupDatabase)
 
 // test for creating the new User
 test('Should create a New User', async () => {
-    const response = await request(server).post('/api/v1/createUser').send({
-        name: 'Piyush Kumar',
-        email: 'piyush@gmail.com',
-        password: 'Piyush@123'
-    }).expect(201)
+    const response = await request(server)
+        .post('/api/v1/createUser')
+        .send({
+            name: 'Piyush Kumar',
+            email: 'piyush@gmail.com',
+            password: 'Piyush@123'
+        })
+        .expect(201)
 
     // Assert that the DB was changed successfully
     const user = await userModel.findById(response.body.message.user._id)
@@ -40,10 +43,13 @@ test('Should create a New User', async () => {
 
 // test for login Process
 test('Should login existing user', async () => {
-    const response = await request(server).post('/api/v1/login').send({
-        email: userOne.email,
-        password: userOne.password
-    }).expect(200)
+    const response = await request(server)
+        .post('/api/v1/login')
+        .send({
+            email: userOne.email,
+            password: userOne.password
+        })
+        .expect(200)
 
     const user = await userModel.findById(userOneId)
     expect(response.body.message.token).toBe(user.tokens[1].token)
@@ -52,10 +58,13 @@ test('Should login existing user', async () => {
 
 // test for authentication failed
 test('Should not login nonexistent user', async () => {
-    await request(server).post('/api/v1/login').send({
+    await request(server)
+        .post('/api/v1/login')
+        .send({
         email: userOne.email,
         password: 'IncorrectPassword'
-    }).expect(400)
+        })
+        .expect(400)
 })
 
 //test for getting the user profile
@@ -74,3 +83,36 @@ test('Should not get profile for unauthenticated user', async () => {
         .send()
         .expect(401)
 })
+
+// test for deleting the user
+test('Should delete the user', async () => {
+    await request(server)
+        .delete('/api/v1/deleteUser')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send()
+        .expect(200)
+    const user = await userModel.findById(userOneId)
+    expect(user).toBeNull()
+})
+
+// test for authentication failed for deleting the user
+test('Should not delete account for unauthenticate user', async () => {
+    await request(server)
+        .delete('/api/v1/deleteUser')
+        .send()
+        .expect(401)
+})
+
+// test for updating the valid user fields
+test('Should update valid user fields', async () => {
+    await request(server)
+        .patch('/api/v1/updateUser')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send({
+            age: '26'
+        })
+        .expect(200)
+    const user = await userModel.findById(userOneId)
+    expect(user.age).toEqual(26)
+})
+
